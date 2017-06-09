c7c693ff5d48fb8e0856c52a0e3eefc1fa4fd6bc
Freezy <jackzhang0096@gmail.com>
2017-06-09 12:30:53 +1000

    Modified colorSection

--- app/src/main/java/edu/monash/fit3027/fit3027_final_application/UI/Activity/colorSelectionGridView.java
+++ app/src/main/java/edu/monash/fit3027/fit3027_final_application/UI/Activity/colorSelectionGridView.java
@@ -1,15 +1,12 @@
 package edu.monash.fit3027.fit3027_final_application.UI.Activity;
 
-import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
-import android.widget.PopupWindow;
 import android.widget.Toast;
 
 import edu.monash.fit3027.fit3027_final_application.R;
@@ -19,24 +16,33 @@ import edu.monash.fit3027.fit3027_final_application.UI.Adapter.colorButtonAdapte
  * Created by Jack on 06-Jun-17.
  */
 
-public class colorSelectionGridView extends AppCompatActivity {
+public class ColorSelectionGridView extends AppCompatActivity implements AdapterView.OnItemClickListener{
 
-
+    private String[] colorSelection;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select_color);
 
+        setTitle("Select Color");
+
+        //Setup back button
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
         Resources res = getResources();
-        String[] colorSelection = res.getStringArray(R.array.colorTagHex);
+        colorSelection = res.getStringArray(R.array.colorTagHex);
         GridView gridview = (GridView) findViewById(R.id.gridview);
         gridview.setAdapter(new colorButtonAdapter(this,colorSelection));
+        gridview.setOnItemClickListener(this);
+    }
 
-        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v,
-                                    int position, long id) {
-                Toast.makeText(colorSelectionGridView.this, "" + position,
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent newIntent = new Intent();
+        newIntent.putExtra("colorTag",colorSelection[position]);
+        setResult(RESULT_OK, newIntent);
+        finish();
     }
 }
